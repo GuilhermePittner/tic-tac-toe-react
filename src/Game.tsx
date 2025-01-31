3859b1d058f94c3f575901223203a3adb20ce72a
guilherme_pittner <guilherme_pittner@ethercity.tech>
2025-01-31 17:25:01 -0300

    finished project

--- src/Game.tsx
+++ src/Game.tsx
@@ -9,34 +9,137 @@ import Square from "./Square";
 /* ================== */
 const GAME_STATE = ["", "", "", "", "", "", "", "", ""]
 
+/* ================== */
+// game matches array,
+// we'll loop this in
+// order to find a winner
+/* ================== */
+const GAME_MATCHES = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+
+type Scores = {
+  [key: string]: number
+}
+
+/* ================== */
+// initial score for
+// each player (0 and 0)
+/* ================== */
+const INITIAL_SCORES: Scores = {
+  X: 0, O: 0
+}
+
 
 function Game() {
 
 
   /* ================== */
   // initializing
-  // game state and 
-  // player hook
+  // game state,
+  // player and
+  // initial score hook
   /* ================== */
   const [gameState, setGameState] = useState(GAME_STATE);
   const [currentPlayer, setCurrentPlayer] = useState("X");
-
-
+  const [score, setScore] = useState(INITIAL_SCORES);
 
 
   /* ================== */
-  // hook to change player 
+  // checking if there's
+  // a score saved on
+  // local storage
+  /* ================== */
+  useEffect(() => {
+    const storedScores = localStorage.getItem("scores");
+    if (storedScores) {
+      setScore(JSON.parse(storedScores));
+    }
+  }, [])
+
+
+  /* ================== */
+  // hook to check victory
+  // and change player 
   // every time "gameState"
   // var is updated. 
   /* ================== */
   useEffect(() => {
-    changePlayer();
+    if (gameState === GAME_STATE){
+      return
+    }
+    checkVictory();
   }, [gameState])
 
   const changePlayer = () => {
     setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
   }
 
+  const checkVictory = () => {
+    let hasWinner = false;
+
+    for (let i = 0; i < GAME_MATCHES.length; i++) {
+      const possibleMatch = GAME_MATCHES[i];
+
+      let first = gameState[possibleMatch[0]]
+      let second = gameState[possibleMatch[1]]
+      let third = gameState[possibleMatch[2]]
+
+      // check if any value is empty
+      if ([first, second, third].includes("")) {
+        continue
+      }
+
+      // check if there's a possible match
+      // if so, break the loop
+      if (first === second && second === third) {
+        hasWinner = true;
+        break
+      }
+    }
+
+    // check for a winner or if it ended in a draw
+
+    if (hasWinner) {
+      setTimeout(() => showWinner(), 500);
+      return
+    }
+
+    if (!gameState.includes("")) {
+      setTimeout(() => showDraw(), 500);
+      return
+    }
+
+    changePlayer();
+  }
+
+  const resetBoard = () => setGameState(GAME_STATE);
+
+  const showWinner = () => {
+    window.alert(`Hey ${currentPlayer} you won.`);
+
+    // update player's score
+    const playerScore = score[currentPlayer] + 1;
+    const newScores = { ...score };
+    newScores[currentPlayer] = playerScore;
+    setScore(newScores);
+    localStorage.setItem("scores", JSON.stringify(newScores));
+
+    resetBoard();
+  }
+
+  const showDraw = () => {
+    window.alert(`There's no winner in this round.`);
+    resetBoard();
+  }
 
 
   /* ================== */
@@ -81,6 +184,12 @@ function Game() {
             />
 
           ))}
+        </div>
+
+        <div className="mx-auto w-96 text-2xl text-serif">
+          <p className="text-white mt-5">It's your turn, <span> {currentPlayer} !</span></p>
+          <p className="text-white mt-5">Player X wins: <span> {score["X"]} </span></p>
+          <p className="text-white mt-5">Player O wins: <span> {score["O"]} </span></p>
         </div>
       </div>
     </div>
